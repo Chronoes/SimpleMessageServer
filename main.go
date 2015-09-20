35b193e572c0f594c79700025fee4d845db1bd63
Chronoes <chronotank@gmail.com>
2015-09-20 13:58:08 +0300

    JSON decoding

--- main.go
+++ main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
-	// "crypto/md5"
-	// "encoding/json"
+	// "crypto/sha1"
+	"encoding/json"
 	// "database/sql"
-	"fmt"
 	// _ "github.com/mattn/go-sqlite3"
-	"io"
+	"log"
 	"net/http"
 )
 
-func RegisterUser(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Register works")
+type User struct {
+	Username, Password, Email string
 }
 
-func LoginUser(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Login works")
+func NewUserFromJSON(req *http.Request) (user *User, err error) {
+	decoder := json.NewDecoder(req.Body)
+	err = decoder.Decode(user)
+	return user, err
+}
+
+func RegisterUser(rw http.ResponseWriter, req *http.Request) {
+	user, err := NewUserFromJSON(req)
+	if err != nil {
+		http.Error(rw, "Error decoding JSON", 400)
+		return
+	}
+	log.Printf("Userdata recieved for registering: %s %s %s\n", user.Username, req.Method, req.URL.String())
+	rw.Write([]byte("Register works"))
+}
+
+func LoginUser(rw http.ResponseWriter, req *http.Request) {
+	user, err := NewUserFromJSON(req)
+	if err != nil {
+		http.Error(rw, "Error decoding JSON", 400)
+		return
+	}
+	log.Printf("Userdata recieved for login: %s\n", user.Username)
+	rw.Write([]byte("Login works"))
 }
 
 func main() {
 	http.HandleFunc("/register", RegisterUser)
 	http.HandleFunc("/login", LoginUser)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Errorf("Server error: %s", err)
-	}
+	log.Println("Server started")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
